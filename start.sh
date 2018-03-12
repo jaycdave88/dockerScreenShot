221fe98adf0db283240d11e18c41d5c496e7b27e
jay.dave <jay.dave@COMP10172.attlocal.net>
2018-03-12 18:51:58 -0500

    updating the start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,19 @@ echo "Starting"
 
 export DOCKERHOST=`/sbin/ip route|awk '/default/ { print $3 }'`
 echo "Docker Host: " $DOCKERHOST
+
 start="$@"
+
+port=$(env | grep _TCP_PORT | cut -d = -f 2)
+
+echo -n "waiting for TCP connection to $host:$port..."
+
+while ! nc -w 1 $DOCKERHOST $port 2>/dev/null
+do
+  echo -n .
+  sleep 1
+done
+
 final=${start/dockerhost/$DOCKERHOST}
 
 /wkhtmltox/bin/wkhtmltoimage ${final}
